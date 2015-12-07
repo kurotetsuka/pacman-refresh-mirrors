bcb0cecaede4e9fedcd50c093f9bd831c77d9cef
kurotetsuka <kurotetsuka@gmail.com>
2015-12-07 12:13:30 -0500

    fixed error exit codes

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 if [[ $(whoami) != "root" ]]; then
 	echo -en "You must be root to install.\n"
-	exit
+	exit 1
 fi
+
+echo -en "Remember to install reflector and rsync!"
+#pacman -S reflector rsync
 
 install -m 755 pacman-refresh-mirrors \
 	/usr/local/sbin/pacman-refresh-mirrors
